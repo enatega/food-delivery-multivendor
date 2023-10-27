fbfcf3023c7128b84aae39f44871e3ec10c494e7
Muhammad Adnan <daniadnan0349@gmail.com>
2023-10-27 12:13:32 +0500

    notification

--- enatega-multivendor-rider/src/routes/index.js
+++ enatega-multivendor-rider/src/routes/index.js
@@ -218,7 +218,7 @@ function AppContainer() {
         })
 
         return {
-          shouldShowAlert: false,
+          shouldShowAlert: true,
           shouldPlaySound: false,
           shouldSetBadge: false
         }
