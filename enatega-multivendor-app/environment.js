d378abb9b7b7dcb04bede97ef073c84d986aa521
Usama Sattar <iamusamasattar@gmail.com>
2023-12-06 23:44:43 +0500

    env files environment file changed

--- enatega-multivendor-app/environment.js
+++ enatega-multivendor-app/environment.js
@@ -12,6 +12,87 @@ const GETCONFIGURATION = gql`
   ${getConfiguration}
 `
 
+let ENV = {}
+
+const getEnvVars = (configuration, env = Updates.releaseChannel) => {
+  // ENV = data.reduce((acc, curr) => {
+  //   return {
+  //     ...acc,
+  //     [curr]: curr.value,
+  //   };
+  // }, {});
+
+  if (env === 'production' || env === 'staging') {
+    const {
+      graphqlUrlApp,
+      wsGraphqlUrlApp,
+      serverUrlApp,
+      androidClientID,
+      iOSClientID,
+      appAmplitudeApiKey,
+      googleApiKey,
+      expoClientID,
+      customerAppSentryUrl,
+      termsAndConditions,
+      privacyPolicy,
+      testOtp
+    } = configuration
+    ENV = {
+      GRAPHQL_URL: graphqlUrlApp,
+      WS_GRAPHQL_URL: wsGraphqlUrlApp,
+      SERVER_URL: serverUrlApp,
+      IOS_CLIENT_ID_GOOGLE: iOSClientID,
+      ANDROID_CLIENT_ID_GOOGLE: androidClientID,
+      AMPLITUDE_API_KEY: appAmplitudeApiKey,
+      GOOGLE_MAPS_KEY: googleApiKey,
+      EXPO_CLIENT_ID: expoClientID,
+      SENTRY_DSN: customerAppSentryUrl,
+      TERMS_AND_CONDITIONS: termsAndConditions,
+      PRIVACY_POLICY: privacyPolicy,
+      TEST_OTP: testOtp
+    }
+    return ENV
+  }
+  return {
+    GRAPHQL_URL: 'http://192.168.18.97:8001/graphql',
+    WS_GRAPHQL_URL: 'ws://192.168.18.97:8001/graphql',
+    SERVER_URL: 'ws://192.168.18.97:8001/',
+    IOS_CLIENT_ID_GOOGLE:
+      '967541328677-30n1b9dljqadrr4badeku41980rf2dt1.apps.googleusercontent.com',
+    ANDROID_CLIENT_ID_GOOGLE:
+      '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
+    AMPLITUDE_API_KEY: '42b182f3d18a21e8ab0ae881c6ef475f',
+    GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
+    EXPO_CLIENT_ID:
+      '967541328677-d46sl62t52g5r3o5m0mnl2hpptr242nl.apps.googleusercontent.com',
+    SENTRY_DSN:
+      'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
+    TERMS_AND_CONDITIONS:
+      'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/terms',
+    PRIVACY_POLICY:
+      'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/privacy',
+    STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
+    STRIPE_IMAGE_URL:
+      'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
+    STRIPE_STORE_NAME: 'Enatega',
+    TEST_OTP: '111111',
+    GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
+  }
+}
+
+export const fetchAndSetEnvVars = async client => {
+  try {
+    const configuration = getConfigData()
+    getEnvVars(configuration)
+  } catch (error) {
+    console.error('Error fetching environment variables:', error)
+  }
+}
+
+export const getEnvVar = () => {
+  return ENV
+}
+
 // const ENV = {
 //   development: {
 //     GRAPHQL_URL: 'https://enatega-multivendor.up.railway.app/graphql',
@@ -115,68 +196,68 @@ const getConfigData = () => {
   return data.configuration
 }
 
-const getEnvVars = async (env = Updates.releaseChannel) => {
-  // What is __DEV__ ?
-  // This variable is set to true when react-native is running in Dev mode.
-  // __DEV__ is true when run locally, but false when published.
-  // eslint-disable-next-line no-undef
+// const getEnvVars = async (env = Updates.releaseChannel) => {
+//   // What is __DEV__ ?
+//   // This variable is set to true when react-native is running in Dev mode.
+//   // __DEV__ is true when run locally, but false when published.
+//   // eslint-disable-next-line no-undef
 
-  const configuration = getConfigData()
-  if (env === 'production' || env === 'staging') {
-    const {
-      graphqlUrlApp,
-      wsGraphqlUrlApp,
-      serverUrlApp,
-      androidClientID,
-      iOSClientID,
-      appAmplitudeApiKey,
-      googleApiKey,
-      expoClientID,
-      customerAppSentryUrl,
-      termsAndConditions,
-      privacyPolicy,
-      testOtp
-    } = configuration
-    return {
-      GRAPHQL_URL: graphqlUrlApp,
-      WS_GRAPHQL_URL: wsGraphqlUrlApp,
-      SERVER_URL: serverUrlApp,
-      IOS_CLIENT_ID_GOOGLE: iOSClientID,
-      ANDROID_CLIENT_ID_GOOGLE: androidClientID,
-      AMPLITUDE_API_KEY: appAmplitudeApiKey,
-      GOOGLE_MAPS_KEY: googleApiKey,
-      EXPO_CLIENT_ID: expoClientID,
-      SENTRY_DSN: customerAppSentryUrl,
-      TERMS_AND_CONDITIONS: termsAndConditions,
-      PRIVACY_POLICY: privacyPolicy,
-      TEST_OTP: testOtp
-    }
-  }
-  return {
-    GRAPHQL_URL: 'http://10.97.39.93:8001/graphql',
-    WS_GRAPHQL_URL: 'ws://10.97.39.93:8001/graphql',
-    SERVER_URL: 'ws://10.97.39.93:8001/',
-    IOS_CLIENT_ID_GOOGLE:
-      '967541328677-30n1b9dljqadrr4badeku41980rf2dt1.apps.googleusercontent.com',
-    ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
-    AMPLITUDE_API_KEY: '42b182f3d18a21e8ab0ae881c6ef475f',
-    GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
-    EXPO_CLIENT_ID:
-      '967541328677-d46sl62t52g5r3o5m0mnl2hpptr242nl.apps.googleusercontent.com',
-    SENTRY_DSN:
-      'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
-    TERMS_AND_CONDITIONS:
-      'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/terms',
-    PRIVACY_POLICY:
-      'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/privacy',
-    STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
-    STRIPE_IMAGE_URL:
-      'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
-    STRIPE_STORE_NAME: 'Enatega',
-    TEST_OTP: '111111',
-    GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
-  }
-}
+//   const configuration = getConfigData()
+//   if (env === 'production' || env === 'staging') {
+//     const {
+//       graphqlUrlApp,
+//       wsGraphqlUrlApp,
+//       serverUrlApp,
+//       androidClientID,
+//       iOSClientID,
+//       appAmplitudeApiKey,
+//       googleApiKey,
+//       expoClientID,
+//       customerAppSentryUrl,
+//       termsAndConditions,
+//       privacyPolicy,
+//       testOtp
+//     } = configuration
+//     return {
+//       GRAPHQL_URL: graphqlUrlApp,
+//       WS_GRAPHQL_URL: wsGraphqlUrlApp,
+//       SERVER_URL: serverUrlApp,
+//       IOS_CLIENT_ID_GOOGLE: iOSClientID,
+//       ANDROID_CLIENT_ID_GOOGLE: androidClientID,
+//       AMPLITUDE_API_KEY: appAmplitudeApiKey,
+//       GOOGLE_MAPS_KEY: googleApiKey,
+//       EXPO_CLIENT_ID: expoClientID,
+//       SENTRY_DSN: customerAppSentryUrl,
+//       TERMS_AND_CONDITIONS: termsAndConditions,
+//       PRIVACY_POLICY: privacyPolicy,
+//       TEST_OTP: testOtp
+//     }
+//   }
+//   return {
+//     GRAPHQL_URL: 'http://10.97.39.93:8001/graphql',
+//     WS_GRAPHQL_URL: 'ws://10.97.39.93:8001/graphql',
+//     SERVER_URL: 'ws://10.97.39.93:8001/',
+//     IOS_CLIENT_ID_GOOGLE:
+//       '967541328677-30n1b9dljqadrr4badeku41980rf2dt1.apps.googleusercontent.com',
+//     ANDROID_CLIENT_ID_GOOGLE:
+//       '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
+//     AMPLITUDE_API_KEY: '42b182f3d18a21e8ab0ae881c6ef475f',
+//     GOOGLE_MAPS_KEY: 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA',
+//     EXPO_CLIENT_ID:
+//       '967541328677-d46sl62t52g5r3o5m0mnl2hpptr242nl.apps.googleusercontent.com',
+//     SENTRY_DSN:
+//       'https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933',
+//     TERMS_AND_CONDITIONS:
+//       'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/terms',
+//     PRIVACY_POLICY:
+//       'https://multivendor.enatega.com/?_gl=1*1yaissu*_ga*MTcwNTMzMjMyOC4xNjk1MTIwMjgz*_ga_DTSL4MVB5L*MTY5NTI3NDgxMC4yLjEuMTY5NTI3NDg0Ni4yNC4wLjA.&_ga=2.41568289.2005342604.1695274812-1705332328.1695120283#/privacy',
+//     STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
+//     STRIPE_IMAGE_URL:
+//       'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
+//     STRIPE_STORE_NAME: 'Enatega',
+//     TEST_OTP: '111111',
+//     GOOGLE_PACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
+//   }
+// }
 
 export default getEnvVars
