793ae8437cd7094f6cd9cb287ace811b0250125c
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2023-12-11 11:30:17 +0500

    i18next ios issue resolved

--- enatega-multivendor-restaurant/i18n.js
+++ enatega-multivendor-restaurant/i18n.js
@@ -1,38 +1,46 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import {sv} from './translations/sv'
-import {en} from './translations/en'
-import {de} from './translations/de'
-import {fr} from './translations/fr'
-import {km} from './translations/km'
-import {zh} from './translations/zh'
-import {ar} from './translations/ar'
+import * as Localization from 'expo-localization'
+import { Platform } from 'react-native'
+import { en } from './translations/en'
+import { de } from './translations/de'
+import { fr } from './translations/fr'
+import { km } from './translations/km'
+import { zh } from './translations/zh'
+import { ar } from './translations/ar'
+import { sv } from './translations/sv'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 export const languageResources = {
-  sv: {translation: sv},
-  en: {translation: en},
-  zh: {translation: zh},
-  de: {translation: de},
-  fr: {translation: fr},
-  km: {translation: km},
-  ar: {translation: ar},
-  ar: {translation: ar}
+  en: { translation: en },
+  zh: { translation: zh },
+  de: { translation: de },
+  fr: { translation: fr },
+  km: { translation: km },
+  ar: { translation: ar },
+  sv: { translation: sv }
 }
-
 const getStoredLanguage = async () => {
   const lng = await AsyncStorage.getItem('enatega-language');
   console.log(lng)
-  
-i18next.use(initReactI18next).init({
-  compatibilityJSON: 'v3',
-  lng: lng,
-  fallbackLng: 'en',
-  resources: languageResources,
-});
-i18next.changeLanguage(lng)
+  i18next.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: lng,
+    fallbackLng: 'en',
+    resources: languageResources,
+  });
 }
-
-getStoredLanguage()
-
-
+if (Platform.OS === 'android') {
+  getStoredLanguage()
+}
+if (Platform.OS === 'ios') {
+  i18next.locale = Localization.locale
+  i18next.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: i18next.locale,
+    fallbackLng: 'en',
+    resources: languageResources,
+  });
+  console.log("language:", Localization.locale)
+  i18next.changeLanguage(i18next.locale)
+}
 export default i18next;
