3a1b4e14da52c2a0dab73f5b9414594c5db144bf
Romana-Aijaz <romanaaijaz99@gmail.com>
2024-01-18 15:54:38 +0500

    change links

--- enatega-multivendor-restaurant/src/utilities/enums.js
+++ enatega-multivendor-restaurant/src/utilities/enums.js
@@ -1,8 +1,8 @@
 export const DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 export const TIMES = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 export const PRODUCT_URL =
-  'https://enatega.com/enatega-multivendor-open-source-food-delivery-solution/'
+  'https://enatega.com/enatega-multi-vendor/'
 export const PRIVACY_URL =
-  'https://multivendor.enatega.com/?_gl=1*gjjx59*_ga*NTczMDY0NDU1LjE2ODUzMzgzODg.*_ga_DTSL4MVB5L*MTY5NjQ4MDQ3Ni41NC4xLjE2OTY0ODA0ODMuNTMuMC4w&_ga=2.17598781.520939582.1696480479-573064455.1685338388#/privacy'
-export const ABOUT_URL = 'https://ninjascode.com/'
+  'https://enatega.com/privacy-policy/'
+export const ABOUT_URL = 'https://ninjascode.com/about-us/'
 export const MAX_TIME = 120
