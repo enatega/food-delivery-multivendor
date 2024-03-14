db809438e386bab221cc9a64ab8e335cf1a3f976
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2024-03-14 11:26:34 +0500

    filter radioBtn color added

--- enatega-multivendor-app/src/components/Filter/FilterSlider.js
+++ enatega-multivendor-app/src/components/Filter/FilterSlider.js
@@ -23,7 +23,7 @@ const Filters = ({ filters, setFilters, applyFilters }) => {
   const [modalVisible, setModalVisible] = useState(false)
   const [selectedFilter, setSelectedFilter] = useState('all')
 
-  const result =filters &&  Object.keys(filters).filter(k =>
+  const result = filters && Object.keys(filters).filter(k =>
     selectedFilter === 'all'
       ? filters[k]
       : selectedFilter === k
@@ -76,7 +76,7 @@ const Filters = ({ filters, setFilters, applyFilters }) => {
           style={[
             styles(currentTheme).filterButton,
             selectedFilter === filter &&
-              styles(currentTheme).selectedFilterButton
+            styles(currentTheme).selectedFilterButton
           ]}
           onPress={() => handleFilterClick(filter)}>
           <SafeAreaView style={styles(currentTheme).itemContainer}>
@@ -108,29 +108,29 @@ const Filters = ({ filters, setFilters, applyFilters }) => {
                       { flexDirection: 'row', justifyContent: 'space-between' },
                       styles(currentTheme).modalItem,
                       filters[filterValue].selected === value &&
-                        styles(currentTheme).selectedModalItem
+                      styles(currentTheme).selectedModalItem
                     ]}
                     onPress={() => handleValueSelection(filterValue, value)}>
                     <Text style={styles(currentTheme).modalItemText}>
                       {value}
                     </Text>
                     {filters && filters[filterValue].type ===
-                    FILTER_TYPE.CHECKBOX ? (
-                        <CheckboxBtn
-                          checked={filters[filterValue].selected.includes(value)}
-                          onPress={() => handleValueSelection(filterValue, value)}
-                        />
-                      ) : (
-                        <RadioButton
-                          size={12}
-                          innerColor={currentTheme.main}
-                          outerColor={currentTheme.radioOuterColor}
-                          isSelected={filters[filterValue].selected.includes(
-                            value
-                          )}
-                          onPress={() => handleValueSelection(filterValue, value)}
-                        />
-                      )}
+                      FILTER_TYPE.CHECKBOX ? (
+                      <CheckboxBtn
+                        checked={filters[filterValue].selected.includes(value)}
+                        onPress={() => handleValueSelection(filterValue, value)}
+                      />
+                    ) : (
+                      <RadioButton
+                        size={12}
+                        innerColor={currentTheme.main}
+                        outerColor={currentTheme.iconColorDark}
+                        isSelected={filters[filterValue].selected.includes(
+                          value
+                        )}
+                        onPress={() => handleValueSelection(filterValue, value)}
+                      />
+                    )}
                   </TouchableOpacity>
                 ))}
               </View>
