35ff937483722f1df0786d596a2c2f0ad392e450
Umer Farooq <umer_farooq@ninjascode.com>
2024-08-16 20:16:37 +0500

    updated sentry - v2

--- enatega-multivendor-web/src/config/constants.js
+++ enatega-multivendor-web/src/config/constants.js
@@ -5,10 +5,10 @@ import ConfigurationContext from "../../src/context/Configuration";
 const ConfigurableValues = () => {
   const configuration = useContext(ConfigurationContext);
 
-  // const SERVER_URL = "https://enatega-multivendor.up.railway.app/";
-  // const WS_SERVER_URL = "wss://enatega-multivendor.up.railway.app/";
-  const SERVER_URL = 'http://192.168.100.15:8001/'
-  const WS_SERVER_URL = 'ws://192.168.100.15:8001/'
+  const SERVER_URL = "https://enatega-multivendor.up.railway.app/";
+  const WS_SERVER_URL = "wss://enatega-multivendor.up.railway.app/";
+  //const SERVER_URL = 'http://192.168.100.15:8001/'
+  //const WS_SERVER_URL = 'ws://192.168.100.15:8001/'
 
   
   const GOOGLE_CLIENT_ID = configuration?.webClientID;
