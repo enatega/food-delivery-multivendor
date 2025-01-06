678a7d70dd2c91957877b8e2cc838f3999a971a4
Ahmed Siddique <ch379796@gmail.com>
2025-01-06 11:44:32 +0500

    changes in firebase files

--- enatega-multivendor-web/src/firebase.js
+++ enatega-multivendor-web/src/firebase.js
@@ -7,13 +7,13 @@ import { getMessaging, isSupported } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAKjJRLT_Dr19k6AANgiH0N4b_gxBueguo",
-  authDomain: "yalla-delivery-405911.firebaseapp.com",
-  projectId: "yalla-delivery-405911",
-  storageBucket: "yalla-delivery-405911.appspot.com",
-  messagingSenderId: "139790486043",
-  appId: "1:139790486043:web:60716d396e0f80e6d086be",
-  measurementId: "G-55T439JHKJ"
+  apiKey: "AIzaSyDx_iSQ9LroTF7NMm20aRvw2wJqhwSnJ3U",
+  authDomain: "enatega-multivender-web.firebaseapp.com",
+  projectId: "enatega-multivender-web",
+  storageBucket: "enatega-multivender-web.appspot.com",
+  messagingSenderId: "438532750182",
+  appId: "1:438532750182:web:516b850eff4e0349f0a6a7",
+  measurementId: "G-KLBJSEHRYQ",
 };
 
 export const initialize = () => {
