eea3aaf0f5cb271e9edeaeadb09b692cc2cd0318
Ahmad Raza <ahmadrazawebexpert@gmail.com>
2025-01-09 10:39:40 +0500

    the width auto adjust enabled in the dashboard screens of both admin and vendor

--- enatega-multivendor-admin/lib/ui/screens/admin/restaurant/dashboard/index.tsx
+++ enatega-multivendor-admin/lib/ui/screens/admin/restaurant/dashboard/index.tsx
@@ -1,28 +1,44 @@
 'use client';
 
-// Component
+// Contexts
+import { LayoutContext } from '@/lib/context/global/layout.context';
+
+// Components
 import GrowthOverView from '@/lib/ui/screen-components/protected/restaurant/dashboard/growth-overview';
 import OrderStats from '@/lib/ui/screen-components/protected/restaurant/dashboard/order-stats';
 import RestaurantStatesTable from '@/lib/ui/screen-components/protected/restaurant/dashboard/restaurant-stats-table';
 import DashboardDateFilter from '@/lib/ui/useable-components/date-filter';
+
+// Interfaces
 import { IDateFilter } from '@/lib/utils/interfaces';
-import { useState } from 'react';
+
+// Hooks
+import { useContext, useState } from 'react';
 
 export default function AdminRestaurantDashboard() {
+  
+  // Contexts
+  const {isSuperAdminSidebarVisible} = useContext(LayoutContext);
+
+  // States
   const [dateFilter, setDateFilter] = useState<IDateFilter>({
     startDate: `${new Date().getFullYear()}-01-01`, // Current year, January 1st
     endDate: `${new Date().getFullYear()}-${String(new Date().getMonth()).padStart(2, '0')}-${String(new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate()).padStart(2, '0')}`, // Last day of previous month
   });
 
   return (
-    <div className="screen-container">
-      <DashboardDateFilter
-        dateFilter={dateFilter}
-        setDateFilter={setDateFilter}
-      />
-      <OrderStats dateFilter={dateFilter} />
-      <GrowthOverView />
-      <RestaurantStatesTable dateFilter={dateFilter} />
-    </div>
+    <>
+      <div
+        className={`flex flex-col ${isSuperAdminSidebarVisible ? 'w-[99%]' : 'w-[100%]'}  overflow-hidden p-3 `}
+      >
+        <DashboardDateFilter
+          dateFilter={dateFilter}
+          setDateFilter={setDateFilter}
+        />
+        <OrderStats dateFilter={dateFilter} />
+        <GrowthOverView />
+        <RestaurantStatesTable dateFilter={dateFilter} />
+      </div>
+    </>
   );
 }
