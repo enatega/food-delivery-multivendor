aff9a16a7b02273c3f8a42359b031afd64630065
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2025-01-16 18:35:54 +0500

    added initial values for admin login and enable ispaid version ui

--- enatega-multivendor-admin/lib/ui/screens/super-admin/management/configurations/index.tsx
+++ enatega-multivendor-admin/lib/ui/screens/super-admin/management/configurations/index.tsx
@@ -1,11 +1,21 @@
 import ConfigHeader from '@/lib/ui/screen-components/protected/super-admin/configuration/view/header';
 import ConfigMain from '@/lib/ui/screen-components/protected/super-admin/configuration/view/main';
+import NoData from '@/lib/ui/useable-components/no-data';
+import { useConfiguration } from '@/lib/hooks/useConfiguration';
 
 export default function ConfigurationsScreen() {
+  const { ISPAID_VERSION } = useConfiguration();
   return (
     <div className="screen-container">
       <ConfigHeader />
-      <ConfigMain />
+      {ISPAID_VERSION ? (
+        <ConfigMain />
+      ) : (
+          <NoData
+            title="Payment Required"
+            message="Please complete your purchase to gain full access to the product."
+          />
+      )}
     </div>
   );
 }
