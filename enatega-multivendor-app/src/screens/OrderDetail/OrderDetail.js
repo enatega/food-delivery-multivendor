7637eabaf1ab1c836026ede62b510c9c42cde611
Ahmed Siddique <ch379796@gmail.com>
2025-01-17 11:21:20 +0500

    order details issues solved

--- enatega-multivendor-app/src/screens/OrderDetail/OrderDetail.js
+++ enatega-multivendor-app/src/screens/OrderDetail/OrderDetail.js
@@ -64,7 +64,6 @@ function OrderDetail(props) {
     onError,
     variables: { abortOrderId: id }
   })
-  
   // useEffect(() => {
   //   /* async function Track() {
   //     await Analytics.track(Analytics.events.NAVIGATE_TO_ORDER_DETAIL, {
@@ -82,10 +81,10 @@ function OrderDetail(props) {
       message: error.message
     })
   }
-
-const order = orders?.find(o => o?._id??order?.id === id) ?? orderData
-//  console.log("IDSSSS:  ", {orderids: orders?.map((o) => o?._id)})
-  // console.log({order, id})
+const order=orders?.find((o)=>
+{
+  return o?._id === id
+})
 
   useEffect(() => {
     props?.navigation.setOptions({
