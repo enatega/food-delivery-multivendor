aa642f5865cd9ee96208efb7c9fe8edbab101712
divyansh <divyanshkathuria4@gmail.com>
2025-02-01 21:50:31 +0530

    modified nameRegex to allow one-letter name

--- enatega-multivendor-app/src/utils/regex.js
+++ enatega-multivendor-app/src/utils/regex.js
@@ -1,6 +1,6 @@
 export const emailRegex = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/
 export const passRegex = /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/
-export const nameRegex = /^[\p{L}][\p{L}\s'-]+$/u
+export const nameRegex = /^[\p{L}][\p{L}\s'-]*$/u
 export const phoneRegex = /^\d{7,15}$/
 
 
