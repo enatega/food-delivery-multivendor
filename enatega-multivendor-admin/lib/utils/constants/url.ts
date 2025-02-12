dfbee775593142477ba995aa59018bcb9ae5f755
Hamza Mushtaq <hamzamushtaq840@gmail.com>
2025-02-12 23:06:56 +0500

    docs(url): change live prod url

--- enatega-multivendor-admin/lib/utils/constants/url.ts
+++ enatega-multivendor-admin/lib/utils/constants/url.ts
@@ -1,10 +1,10 @@
 export const BACKEND_URL = {
   LOCAL: {
-    SERVER_URL: 'http://localhost:8001/', //'https://enatega-multivendor.up.railway.app/',
+    SERVER_URL: 'http://localhost:8001/', //'https://enatega-multivendor-revamp.up.railway.app/graphql',
     WS_SERVER_URL: 'ws://localhost:8001/', //wss://enatega-multivendor.up.railway.app/',
   },
   LIVE: {
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/',
-    WS_SERVER_URL: 'wss://enatega-multivendor.up.railway.app/',
+    SERVER_URL: 'https://enatega-multivendor-revamp.up.railway.app/',
+    WS_SERVER_URL: 'wss://enatega-multivendor-revamp.up.railway.app/',
   },
 };
