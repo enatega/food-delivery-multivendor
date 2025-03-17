8e4e756f75ae0c6f3aa7e78749c4cc040531880c
Faizan1421 <faizantayyab37@gmail.com>
2025-03-17 14:11:51 +0500

    fixed rider phone validation

--- enatega-multivendor-admin/lib/utils/constants/strings/error-messages.ts
+++ enatega-multivendor-admin/lib/utils/constants/strings/error-messages.ts
@@ -105,7 +105,7 @@ export const RiderErrors: IRiderErrors = {
   password: ['Required', ...PasswordErrors],
   confirmPassword: ['Required', 'Password must match'],
   zone: ['Required'],
-  phone: ['Required'],
+  phone: ['Required','Minimum 5 Numbers are Required'],
   vehicleType: ['Required'],
 };
 
