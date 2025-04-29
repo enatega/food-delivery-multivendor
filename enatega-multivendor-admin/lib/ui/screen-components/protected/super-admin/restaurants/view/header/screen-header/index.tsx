458a1eab44d51f34a9192d3f95631bec1aa5267d
Umer Farooq <umer_farooq@ninjascode.com>
2025-04-29 10:41:10 +0500

    main-single-vendor - added checks for single vendor - v2

--- enatega-multivendor-admin/lib/ui/screen-components/protected/super-admin/restaurants/view/header/screen-header/index.tsx
+++ enatega-multivendor-admin/lib/ui/screen-components/protected/super-admin/restaurants/view/header/screen-header/index.tsx
@@ -13,24 +13,33 @@ import TextIconClickable from '@/lib/ui/useable-components/text-icon-clickable';
 // Icon imports
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { useTranslations } from 'next-intl';
+import { useConfiguration } from '@/lib/hooks/useConfiguration';
 
 export default function RestaurantsScreenHeader() {
   // Hooks
   const t = useTranslations();
-
   // Context
   const { onRestaurantsFormVisible } = useContext(RestaurantsContext);
+
+  // Hooks
+  const { IS_MULTIVENDOR, RESTURANT_COUNT } = useConfiguration();
+
+  // Constants
+  const IS_NOT_ALLOWED_MORE = !IS_MULTIVENDOR && (RESTURANT_COUNT || 0) >= 1;
+
   return (
     <div className="sticky top-0 z-10 w-full flex-shrink-0 bg-white p-3 shadow-sm">
       <div className="flex w-full justify-between">
         <HeaderText text={t('Stores')} />
-        <TextIconClickable
-          className="rounded border-gray-300 bg-black text-white sm:w-auto"
-          icon={faAdd}
-          iconStyles={{ color: 'white' }}
-          title={t('Add Store')}
-          onClick={() => onRestaurantsFormVisible(true)}
-        />
+        {!IS_NOT_ALLOWED_MORE && (
+          <TextIconClickable
+            className="rounded border-gray-300 bg-black text-white sm:w-auto"
+            icon={faAdd}
+            iconStyles={{ color: 'white' }}
+            title={t('Add Store')}
+            onClick={() => onRestaurantsFormVisible(true)}
+          />
+        )}
       </div>
     </div>
   );
