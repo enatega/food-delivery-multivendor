c44cf97c6aed98d6566a377091184f46f64fe12f
Umer F <uf.umerfarooq67@gmail.com>
2025-05-27 14:49:03 +0500

    updated admin with staging backend url

--- enatega-multivendor-admin/lib/utils/constants/url.ts
+++ enatega-multivendor-admin/lib/utils/constants/url.ts
@@ -1,12 +1,10 @@
 export const BACKEND_URL = {
   LOCAL: {
-    SERVER_URL: 'http://localhost:8001/', //'https://enatega-multivendor.up.railway.app/',
-    WS_SERVER_URL: 'ws://localhost:8001/', //wss://enatega-multivendor.up.railway.app/',
+    SERVER_URL: 'http://localhost:8001/',
+    WS_SERVER_URL: 'ws://localhost:8001/',
   },
   LIVE: {
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/',
-    WS_SERVER_URL: 'wss://enatega-multivendor.up.railway.app/',
-    // SERVER_URL: 'https://enatega-multivendor-revamp.up.railway.app/',
-    // WS_SERVER_URL: 'wss://enatega-multivendor-revamp.up.railway.app/',
+    SERVER_URL: 'https://v1-api-enatega-multivendor-stage.up.railway.app/',
+    WS_SERVER_URL: 'wss://v1-api-enatega-multivendor-stage.up.railway.app/',
   },
 };
