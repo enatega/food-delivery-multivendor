01c1b4967862abfe07975d0cd0621f791c3061f1
Umer F <uf.umerfarooq67@gmail.com>
2025-05-27 14:52:04 +0500

    updated backend url in develop branch

--- enatega-multivendor-admin/lib/utils/constants/url.ts
+++ enatega-multivendor-admin/lib/utils/constants/url.ts
@@ -1,12 +1,10 @@
 export const BACKEND_URL = {
   LOCAL: {
-    SERVER_URL: 'http://localhost:8001/', //'https://enatega-multivendor.up.railway.app/',
-    WS_SERVER_URL: 'ws://localhost:8001/', //wss://enatega-multivendor.up.railway.app/',
+    SERVER_URL: 'http://localhost:8001/',
+    WS_SERVER_URL: 'ws://localhost:8001/',
   },
   LIVE: {
-    SERVER_URL: 'https://enatega-multivendor.up.railway.app/',
-    WS_SERVER_URL: 'wss://enatega-multivendor.up.railway.app/',
-    // SERVER_URL: 'https://enatega-multivendor-revamp.up.railway.app/',
-    // WS_SERVER_URL: 'wss://enatega-multivendor-revamp.up.railway.app/',
+    SERVER_URL: 'https://https://v1-api-enatega-multivendor-develop.up.railway.app/',
+    WS_SERVER_URL: 'wss://https://v1-api-enatega-multivendor-develop.up.railway.app/',
   },
 };
