ff2e8dec816183a737a5d8a0165ba79b615568c2
Umer F <uf.umerfarooq67@gmail.com>
2025-06-02 16:34:00 +0500

    disabled shoptypes

--- enatega-multivendor-admin/app/(localized)/(protected)/(super-admin)/management/shop-types/page.tsx
+++ enatega-multivendor-admin/app/(localized)/(protected)/(super-admin)/management/shop-types/page.tsx
@@ -2,7 +2,9 @@
 
 // Screens
 import ShopTypesScreen from '@/lib/ui/screens/super-admin/management/shop-types';
+import ComingSoon from '@/lib/ui/useable-components/coming-soon';
 
 export default function ShopeTypesPage() {
-  return <ShopTypesScreen />;
+  // return <ShopTypesScreen />;
+  return <ComingSoon />;
 }
