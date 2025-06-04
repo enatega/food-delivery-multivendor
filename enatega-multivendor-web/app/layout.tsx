af9101a435ffacc43ff88f7fa3f915e1c95132dd
Umer F <uf.umerfarooq67@gmail.com>
2025-06-04 18:04:03 +0500

    commneted pwa component

--- enatega-multivendor-web/app/layout.tsx
+++ enatega-multivendor-web/app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
-          <InstallPWA/>
+          {/* <InstallPWA/> */}
         </NextIntlClientProvider>
       </body>
     </html>
