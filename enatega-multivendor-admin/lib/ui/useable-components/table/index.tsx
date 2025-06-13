af39781b43be5841cb0ae59e9007f6c97aa334db
Umer Farooq <34151564+ufumerfarooq67@users.noreply.github.com>
2025-06-13 11:33:06 +0500

    Fix Enatega Admin dashboard bugs (#1302) (#1308)
    * updated admin url to main backend api url
    
    * merge env changes in main from stage (#1290)
    
    * updated admin with staging backend url
    
    * updated backend url in develop branch
    
    * updated backend url in develop branch - v2
    
    * replaced https with wss in ws server url
    
    * added envs
    
    * added stage env
    
    * added envs
    
    * added envs - v2
    
    * fixed dispatch table sorting on client side
    
    * Fix enatega admin dashboard bugs
    
    ---------
    
    Co-authored-by: Zeeshan Rafiq <102378715+ZeeshanRafiq38@users.noreply.github.com>

--- enatega-multivendor-admin/lib/ui/useable-components/table/index.tsx
+++ enatega-multivendor-admin/lib/ui/useable-components/table/index.tsx
@@ -30,8 +30,6 @@ const Table = <T extends ITableExtends>({
   onPage,
   className,
   // For Store table
-  sortField,
-  sortOrder,
   scrollable = true,
   scrollHeight = '420px',
   // New pagination props
@@ -75,14 +73,8 @@ const Table = <T extends ITableExtends>({
   };
 
   // Prepare pagination props based on server pagination status
-  const paginationProps = isServerPaginated
-    ? {
-        first: (currentPage - 1) * rowsPerPage,
-        lazy: true,
-        totalRecords,
-        onPage: handlePageChange,
-      }
-    : {};
+  // Remove lazy and first for client-side sorting/pagination
+  const paginationProps = {};
 
   return (
     <>
@@ -90,10 +82,7 @@ const Table = <T extends ITableExtends>({
         header={header}
         paginator
         rows={rowsPerPage}
-        sortField={sortField}
-        sortOrder={sortOrder}
         rowsPerPageOptions={[10, 15, 25, 50]}
-        onPage={onPage}
         value={data}
         selectionAutoFocus={true}
         size={size}
@@ -114,7 +103,7 @@ const Table = <T extends ITableExtends>({
         rowClassName={rowClassName}
         onRowClick={handleRowClick}
         emptyMessage={t('No Data Available')}
-        {...paginationProps} // Spread pagination props conditionally
+        {...paginationProps}
       >
         {isSelectable && (
           <Column
