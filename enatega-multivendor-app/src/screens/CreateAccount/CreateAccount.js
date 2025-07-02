95a4ec607939f6df47b0cfbd3c753dde9e34541e
NaeemUlHaq1274 <naeemcharbagh1274@gmail.com>
2025-07-02 16:33:53 +0500

    changes

--- enatega-multivendor-app/src/screens/CreateAccount/CreateAccount.js
+++ enatega-multivendor-app/src/screens/CreateAccount/CreateAccount.js
@@ -59,32 +59,6 @@ const CreateAccount = (props) => {
         </View>
       )
     }
-
-    // <View style={styles.container}>
-    //   <AppleAuthentication.AppleAuthenticationButton
-    //     buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-    //     buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-    //     cornerRadius={5}
-    //     style={styles.button}
-    //     onPress={async () => {
-    //       try {
-    //         const credential = await AppleAuthentication.signInAsync({
-    //           requestedScopes: [
-    //             AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-    //             AppleAuthentication.AppleAuthenticationScope.EMAIL,
-    //           ],
-    //         });
-    //         // signed in
-    //       } catch (e) {
-    //         if (e.code === 'ERR_REQUEST_CANCELED') {
-    //           // handle that the user canceled the sign-in flow
-    //         } else {
-    //           // handle other errors
-    //         }
-    //       }
-    //     }}
-    //   />
-    // </View>
     return (
       <AppleAuthentication.AppleAuthenticationButton
         buttonType={AppleAuthentication.AppleAuthenticationButtonType.CONTINUE}
