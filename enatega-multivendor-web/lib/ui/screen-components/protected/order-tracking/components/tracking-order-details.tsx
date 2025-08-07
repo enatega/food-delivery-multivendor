f24ec25ed13905f1c000a8719e18a0d555ccb724
AdiiiCode <adeel2694551@gmail.com>
2025-08-07 15:41:22 +0500

    Translation Changes

--- enatega-multivendor-web/lib/ui/screen-components/protected/order-tracking/components/tracking-order-details.tsx
+++ enatega-multivendor-web/lib/ui/screen-components/protected/order-tracking/components/tracking-order-details.tsx
@@ -201,7 +201,7 @@ console.log(orderTrackingDetails)
 
           {calculateTotalAddonPrice() > 0 && (
           <div className="flex justify-between">
-            <span>{t("Addons")}</span>
+            <span>{t("Addons_label")}</span>
             <span>
               {formatCurrency(calculateTotalAddonPrice() || 0)}
             </span>
