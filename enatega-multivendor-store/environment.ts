e57aa23ee13a89e0fa87725df26883aec4cb8e0e
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2025-08-26 15:20:03 +0500

    Update app versions and improve environment variable handling

--- enatega-multivendor-store/environment.ts
+++ enatega-multivendor-store/environment.ts
@@ -12,18 +12,18 @@ const getEnvVars = (env = Updates.channel) => {
 
   if (env === "production" || env === "staging") {
     return {
-      GRAPHQL_URL: "https://enatega-multivendor-revamp.up.railway.app/graphql",
-      WS_GRAPHQL_URL: "wss://enatega-multivendor-revamp.up.railway.app/graphql",
+      GRAPHQL_URL: "https://enatega-multivendor.up.railway.app/graphql",
+      WS_GRAPHQL_URL: "wss://enatega-multivendor.up.railway.app/graphql",
       SENTRY_DSN:
         configuration?.restaurantAppSentryUrl ??
         "https://91b55f514a2c4708845789d6e79abf10@o1103026.ingest.sentry.io/6131933",
     };
   }
   return {
-    GRAPHQL_URL: "http://192.168.1.21:8001/graphql",
-    WS_GRAPHQL_URL: "ws://192.168.1.21:8001/graphql",
-    // GRAPHQL_URL: "https://enatega-multivendor-revamp.up.railway.app/graphql",
-    // WS_GRAPHQL_URL: "wss://enatega-multivendor-revamp.up.railway.app/graphql",
+    // GRAPHQL_URL: "http://192.168.1.21:8001/graphql",
+    // WS_GRAPHQL_URL: "ws://192.168.1.21:8001/graphql",
+    GRAPHQL_URL: "https://enatega-multivendor.up.railway.app/graphql",
+    WS_GRAPHQL_URL: "wss://enatega-multivendor.up.railway.app/graphql",
 
     SENTRY_DSN:
       configuration?.restaurantAppSentryUrl ??
