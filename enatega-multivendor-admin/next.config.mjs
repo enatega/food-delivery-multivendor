0c602e1d066bb8d3128ae41856ec5bd3d8ad3071
faizan tayyab <faizantayyab37@gmail.com>
2025-09-04 17:09:22 +0500

    added  unoptimized: true in nextconfig for fixing 502 error

--- enatega-multivendor-admin/next.config.mjs
+++ enatega-multivendor-admin/next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
