1aa31dec0f486f3db81877ff3aeeccd406e63524
faizan tayyab <faizantayyab37@gmail.com>
2025-10-02 13:48:56 +0500

    fixed-Favorite button in top-right corner opens restaurant instead of adding to favorites #1506

--- enatega-multivendor-app/src/components/Main/RestaurantCard/NewRestaurantCard.js
+++ enatega-multivendor-app/src/components/Main/RestaurantCard/NewRestaurantCard.js
@@ -101,111 +101,115 @@ function NewRestaurantCard(props) {
     }
   }
   return (
-    <Ripple 
-      rippleColor={'#F5F5F5'} 
-      style={[
-        styles(currentTheme).offerContainer, 
-        props?.fullWidth && { width: '100%' },
-        Platform.OS === 'android' && {
-          overflow: 'hidden',
-          borderRadius: 15
-        }
-      ]} 
-      activeOpacity={0.8} 
-      onPress={handleRestaurantClick}
-      rippleContainerBorderRadius={15}
-      rippleDuration={Platform.OS === 'android' ? 300 : 400}
-      rippleSize={Platform.OS === 'android' ? 150 : 200}
-      disabled={false}
-    >
-      <View
+    <View style={[
+      styles(currentTheme).offerContainer, 
+      props?.fullWidth && { width: '100%' },
+      { position: 'relative' }
+    ]}>
+      <Ripple 
+        rippleColor={'#F5F5F5'} 
         style={[
-          styles().container,
-          themeContext.ThemeValue === 'Pink' && {
-            backgroundColor: 'white',
-            borderRadius: 8,
-
-            // iOS shadow
-            shadowColor: '#000',
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.05,
-            shadowRadius: 8,
-            // Android shadow
-            elevation: 2,
-            marginBottom: 5
+          { width: '100%', height: '100%' },
+          Platform.OS === 'android' && {
+            overflow: 'hidden',
+            borderRadius: 15
           }
-        ]}
+        ]} 
+        activeOpacity={0.8} 
+        onPress={handleRestaurantClick}
+        rippleContainerBorderRadius={15}
+        rippleDuration={Platform.OS === 'android' ? 300 : 400}
+        rippleSize={Platform.OS === 'android' ? 150 : 200}
+        disabled={false}
       >
-        <View style={styles().imageContainer}>
-          <Image resizeMode='cover' source={{ uri: props?.image }} style={[styles().restaurantImage, props?.fullWidth && { width: '100%' }]} />
-          {isRestaurantClosed && (
-            <View style={styles(currentTheme).closedOverlay}>
-              <TextDefault H4 textColor={currentTheme.white} bold>
-                Closed
-              </TextDefault>
-            </View>
-          )}
-        </View>
-        <View style={styles().descriptionContainer}>
-          <View style={styles(currentTheme).aboutRestaurant}>
-            <TextDefault H4 numberOfLines={1} textColor={currentTheme.fontThirdColor} bolder>
-              {props?.name}
-            </TextDefault>
-          </View>
-          <TextDefault textColor={currentTheme.gray600} numberOfLines={1} bold Normal style={styles(currentTheme).offerCategoty}>
-            {props?.categories ? props?.categories.map((category) => category?.title + ', ') : props?.tags?.join(',')}
-          </TextDefault>
-          <View style={styles().border} />
-          <View style={styles(currentTheme).deliveryInfo}>
-            <View style={styles(currentTheme).deliveryTime}>
-              <AntDesign name='clockcircleo' size={14} color={currentTheme.editProfileButton} />
-              <TextDefault textColor={currentTheme.editProfileButton} numberOfLines={1} bold Normal>
-                {props?.deliveryTime + ' '}
-                {t('min')}
-              </TextDefault>
-            </View>
-            <View style={styles(currentTheme).deliveryTime}>
-              <Bicycle color={currentTheme.newFontcolor} />
-              <TextDefault textColor={currentTheme.newFontcolor} numberOfLines={1} bold Normal>
-                {configuration.currencySymbol} {configuration.deliveryRate}
-              </TextDefault>
-            </View>
-            <View style={styles(currentTheme).aboutRestaurant}>
-              <FontAwesome5 name='star' size={14} color={currentTheme.newFontcolor} />
-              <TextDefault textColor={currentTheme.newFontcolor} bold Normal>
-                {props?.reviewAverage}
-              </TextDefault>
-              <TextDefault textColor={currentTheme.newFontcolor} bold Normal>
-                ({props?.reviewCount})
-              </TextDefault>
-            </View>
-          </View>
-        </View>
-      </View>
-      <View style={[styles().overlayContainer, props?.fullWidth && { width: '100%' }]}>
-        <TouchableOpacity 
-          activeOpacity={0.7} 
-          disabled={loadingMutation} 
-          onPress={handleAddToFavorites}
-          hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-          style={{ 
-            position: 'absolute',
-            top: 10,
-            ...(currentTheme?.isRTL ? { left: 10 } : { right: 10 }),
-            width: scale(38),
-            height: scale(28),
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 1000,
-            pointerEvents: 'auto'
-          }}
+        <View
+          style={[
+            styles().container,
+            themeContext.ThemeValue === 'Pink' && {
+              backgroundColor: 'white',
+              borderRadius: 8,
+
+              // iOS shadow
+              shadowColor: '#000',
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.05,
+              shadowRadius: 8,
+              // Android shadow
+              elevation: 2,
+              marginBottom: 5
+            }
+          ]}
         >
-          <View style={styles(currentTheme).favouriteOverlay}>
-            {loadingMutation ? <Spinner size={'small'} backColor={'transparent'} spinnerColor={currentTheme.iconColorDark} /> : <AntDesign name={heart ? 'heart' : 'hearto'} size={scale(15)} color={currentTheme.iconColor} />}
+          <View style={styles().imageContainer}>
+            <Image resizeMode='cover' source={{ uri: props?.image }} style={[styles().restaurantImage, props?.fullWidth && { width: '100%' }]} />
+            {isRestaurantClosed && (
+              <View style={styles(currentTheme).closedOverlay}>
+                <TextDefault H4 textColor={currentTheme.white} bold>
+                  Closed
+                </TextDefault>
+              </View>
+            )}
           </View>
-        </TouchableOpacity>
-      </View>
-    </Ripple>
+          <View style={styles().descriptionContainer}>
+            <View style={styles(currentTheme).aboutRestaurant}>
+              <TextDefault H4 numberOfLines={1} textColor={currentTheme.fontThirdColor} bolder>
+                {props?.name}
+              </TextDefault>
+            </View>
+            <TextDefault textColor={currentTheme.gray600} numberOfLines={1} bold Normal style={styles(currentTheme).offerCategoty}>
+              {props?.categories ? props?.categories.map((category) => category?.title + ', ') : props?.tags?.join(',')}
+            </TextDefault>
+            <View style={styles().border} />
+            <View style={styles(currentTheme).deliveryInfo}>
+              <View style={styles(currentTheme).deliveryTime}>
+                <AntDesign name='clockcircleo' size={14} color={currentTheme.editProfileButton} />
+                <TextDefault textColor={currentTheme.editProfileButton} numberOfLines={1} bold Normal>
+                  {props?.deliveryTime + ' '}
+                  {t('min')}
+                </TextDefault>
+              </View>
+              <View style={styles(currentTheme).deliveryTime}>
+                <Bicycle color={currentTheme.newFontcolor} />
+                <TextDefault textColor={currentTheme.newFontcolor} numberOfLines={1} bold Normal>
+                  {configuration.currencySymbol} {configuration.deliveryRate}
+                </TextDefault>
+              </View>
+              <View style={styles(currentTheme).aboutRestaurant}>
+                <FontAwesome5 name='star' size={14} color={currentTheme.newFontcolor} />
+                <TextDefault textColor={currentTheme.newFontcolor} bold Normal>
+                  {props?.reviewAverage}
+                </TextDefault>
+                <TextDefault textColor={currentTheme.newFontcolor} bold Normal>
+                  ({props?.reviewCount})
+                </TextDefault>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Ripple>
+      
+      <TouchableOpacity 
+        activeOpacity={0.7} 
+        disabled={loadingMutation} 
+        onPress={handleAddToFavorites}
+        hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+        style={{ 
+          position: 'absolute',
+          top: 10,
+          ...(currentTheme?.isRTL ? { left: 10 } : { right: 10 }),
+          width: scale(38),
+          height: scale(28),
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 1000,
+          elevation: 1000
+        }}
+      >
+        <View style={styles(currentTheme).favouriteOverlay}>
+          {loadingMutation ? <Spinner size={'small'} backColor={'transparent'} spinnerColor={currentTheme.iconColorDark} /> : <AntDesign name={heart ? 'heart' : 'hearto'} size={scale(15)} color={currentTheme.iconColor} />}
+        </View>
+      </TouchableOpacity>
+    </View>
   )
 }
 
