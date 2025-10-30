f405f2eb35c1ace4e905c2bf41c2ad5a5fb65b32
Umer F <uf.umerfarooq67@gmail.com>
2025-10-30 10:27:59 +0500

    feat: added all languages to i18 in store app

--- enatega-multivendor-store/i18next.ts
+++ enatega-multivendor-store/i18next.ts
@@ -12,6 +12,30 @@ import { km } from "./languages/km";
 import { zh } from "./languages/zh";
 import { ar } from "./languages/ar";
 import { he } from "./languages/he";
+import { hi } from "./languages/hi";
+import { es } from "./languages/es";
+import { bn } from "./languages/bn";
+import { pt } from "./languages/pt";
+import { ru } from "./languages/ru";
+import { ur } from "./languages/ur";
+import { id } from "./languages/id";
+import { jp } from "./languages/jp";
+import { tr } from "./languages/tr";
+import { mr } from "./languages/mr";
+import { te } from "./languages/te";
+import { vi } from "./languages/vi";
+import { ko } from "./languages/ko";
+import { it } from "./languages/it";
+import { th } from "./languages/th";
+import { gu } from "./languages/gu";
+import { fa } from "./languages/fa";
+import { pl } from "./languages/pl";
+import { ps } from "./languages/ps";
+import { ro } from "./languages/ro";
+import { ku } from "./languages/ku";
+import { uz } from "./languages/uz";
+import { az } from "./languages/az";
+import { nl } from "./languages/nl";
 
 // Define language resources
 export const languageResources: { [key: string]: { translation: object } } = {
@@ -22,6 +46,30 @@ export const languageResources: { [key: string]: { translation: object } } = {
   km: { translation: km },
   ar: { translation: ar },
   he: { translation: he },
+  hi: { translation: hi },
+  es: { translation: es },
+  bn: { translation: bn },
+  pt: { translation: pt },
+  ru: { translation: ru },
+  ur: { translation: ur },
+  id: { translation: id },
+  jp: { translation: jp },
+  tr: { translation: tr },
+  mr: { translation: mr },
+  te: { translation: te },
+  vi: { translation: vi },
+  ko: { translation: ko },
+  it: { translation: it },
+  th: { translation: th },
+  gu: { translation: gu },
+  fa: { translation: fa },
+  pl: { translation: pl },
+  ps: { translation: ps },
+  ro: { translation: ro },
+  ku: { translation: ku },
+  uz: { translation: uz },
+  az: { translation: az },
+  nl: { translation: nl },
 };
 
 // Function to get stored language from AsyncStorage or fallback to device locale
