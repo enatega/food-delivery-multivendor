c0ec47dfc767a5eb4a422d601961b723cd2aec6c
Zeeshan Rafiq <xeeshanrafiq38@gmail.com>
2025-11-17 13:08:16 +0500

    update next config aws s3 url

--- enatega-multivendor-web/next.config.mjs
+++ enatega-multivendor-web/next.config.mjs
@@ -132,7 +132,7 @@ const nextConfig = {
       },
        {
         protocol: 'https',
-        hostname: 'enatega-backend.s3.eu-north-1.amazonaws.com',
+        hostname: 'un2x3-bucket.s3.us-east-2.amazonaws.com',
       },
       {
         protocol: 'https',
