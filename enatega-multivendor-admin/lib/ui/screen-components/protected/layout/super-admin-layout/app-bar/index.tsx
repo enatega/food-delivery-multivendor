6b57d06d43092b8e52505e087759d18a94b207a6
Abdullah <abdullahzahid903126@gmail.com>
2025-12-05 12:50:25 +0500

    updated tailwind config color variables

--- enatega-multivendor-admin/lib/ui/screen-components/protected/layout/super-admin-layout/app-bar/index.tsx
+++ enatega-multivendor-admin/lib/ui/screen-components/protected/layout/super-admin-layout/app-bar/index.tsx
@@ -222,7 +222,7 @@ const AppTopbar = () => {
   }));
 
   return (
-    <div className={`${classes['layout-topbar']}`}>
+    <div className={` dark:bg-dark-950 dark:text-white dark:border-dark-600 ${classes['layout-topbar']}`}>
       <div className="flex items-center cursor-pointer">
         <div id="sidebar-opening-icon">
           <button onClick={() => showSuperAdminSidebar()}>
--- enatega-multivendor-admin/tailwind.config.ts
+++ enatega-multivendor-admin/tailwind.config.ts
@@ -32,7 +32,7 @@ const config = {
         'secondary-border-color': '#111827',
         'dark-950': 'var(--dark-950)',
         'dark-900': 'var(--dark-900)',
-        'border-dark-600': 'var(--border-dark-600)',
+        'dark-600': 'var(--dark-600)',
       },
       width: {
         'custom-button': '110px',
