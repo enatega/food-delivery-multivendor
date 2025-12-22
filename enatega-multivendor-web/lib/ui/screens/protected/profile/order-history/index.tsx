80f60c310f9d389d64530522d0e2e8758f73ac5b
Abdullah <abdullahzahid903126@gmail.com>
2025-12-22 15:25:00 +0500

    feat: Add GraphQL queries for user past and active orders and update order history screen

--- enatega-multivendor-web/lib/ui/screens/protected/profile/order-history/index.tsx
+++ enatega-multivendor-web/lib/ui/screens/protected/profile/order-history/index.tsx
@@ -1,57 +1,105 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { ORDERS } from "@/lib/api/graphql/queries/orders";
+import {
+  GET_USERS_ACTIVE_ORDERS,
+  GET_USERS_PAST_ORDERS,
+} from "@/lib/api/graphql/queries/orders";
 import {
   ActiveOrders,
   PastOrders,
 } from "@/lib/ui/screen-components/protected/profile";
-import { ACTIVE_STATUS, INACTIVE_STATUS } from "@/lib/utils/constants/orders";
 import { IOrder } from "@/lib/utils/interfaces/orders.interface";
+import { useTranslations } from "next-intl";
 
 export default function OrderHistoryScreen() {
   const [page, setPage] = useState(1);
-  const [allOrders, setAllOrders] = useState<IOrder[]>([]);
-  const [hasMore, setHasMore] = useState(true);
-  const limit = 10;
+  const [activeOrders, setActiveOrders] = useState<IOrder[]>([]);
+  const [pastOrders, setPastOrders] = useState<IOrder[]>([]);
+  const [activeOrderhasMore, setActiveOrderHasMore] = useState(true);
+  const [pastOrderhasMore, setPastOrderHasMore] = useState(true);
+  const limit = 5;
+  const t = useTranslations()
 
-  const { data, loading, fetchMore, networkStatus } = useQuery(ORDERS, {
-    variables: { page, limit },
-    fetchPolicy: "cache-and-network",
-    notifyOnNetworkStatusChange: true,
-  });
+  // const { data, loading, fetchMore, networkStatus } = useQuery(ORDERS, {
+  //   variables: { page, limit },
+  //   fetchPolicy: "cache-and-network",
+  //   notifyOnNetworkStatusChange: true,
+  // });
+
+  const { data:pastOrder, loading:pastOrderLoading, fetchMore:pastOrderFetchMore, networkStatus:pastOrderNetwork } = useQuery(
+    GET_USERS_PAST_ORDERS,
+    {
+      variables: {
+        page,
+        limit,
+        offset: 0,
+      },
+    }
+  );
+
+    const { data:activeOrder, loading:activeOrderLoading, fetchMore:activeOrderFetchMore, networkStatus:activeOrderNetwork } = useQuery(
+    GET_USERS_ACTIVE_ORDERS,
+    {
+      variables: {
+        page,
+        limit,
+        offset: 0,
+      },
+    }
+  );
 
   // Merge new orders & update hasMore
   useEffect(() => {
-    if (data?.orders) {
-      setAllOrders((prev) => {
-        const newOrders = data.orders.filter(
+    if (activeOrder?.getUsersActiveOrders || pastOrder?.getUsersPastOrders) {
+      // setAllOrders((prev) => {
+      //   const newOrders = data.orders.filter(
+      //     (order: IOrder) => !prev.some((p) => p._id === order._id)
+      //   );
+      //   return [...prev, ...newOrders];
+      // });
+       setActiveOrders((prev) => {
+        const newActiveOrders = activeOrder?.getUsersActiveOrders?.filter(
           (order: IOrder) => !prev.some((p) => p._id === order._id)
-        );
-        return [...prev, ...newOrders];
+        ) ?? [];
+        return [...prev, ...newActiveOrders];
       });
 
-      if (data.orders.length < limit) {
-        setHasMore(false);
+       setPastOrders((prev) => {
+        const newPastOrders = pastOrder?.getUsersPastOrders?.filter(
+          (order: IOrder) => !prev.some((p) => p._id === order._id)
+        )?? [];
+        return [...prev, ...newPastOrders];
+      });
+
+
+      if (activeOrder?.getUsersActiveOrders?.length < limit ) {
+        setActiveOrderHasMore(false);
+      }
+      if (pastOrder?.getUsersPastOrders?.length < limit ) {
+        setPastOrderHasMore(false);
       }
     }
-  }, [data]);
+  }, [activeOrder,pastOrder]);
 
-  const activeOrders = allOrders.filter((o) =>
-    ACTIVE_STATUS.includes(o.orderStatus)
-  );
-  const pastOrders = allOrders.filter((o) =>
-    INACTIVE_STATUS.includes(o.orderStatus)
-  );
+  // const activeOrders = allOrders.filter((o) =>
+  //   ACTIVE_STATUS.includes(o.orderStatus)
+  // );
+  // const pastOrders = allOrders.filter((o) =>
+  //   INACTIVE_STATUS.includes(o.orderStatus)
+  // );
 
   const loadMore = () => {
-    if (!hasMore) return;
+    if (!activeOrderhasMore && !pastOrderhasMore) return;
 
     const nextPage = page + 1;
     setPage(nextPage);
 
-    fetchMore({
-      variables: { page: nextPage, limit },
+    activeOrderFetchMore({
+      variables: { page: nextPage, limit,offset: 0 },
+    });
+     pastOrderFetchMore({
+      variables: { page: nextPage, limit,offset: 0 },
     });
   };
 
@@ -60,24 +108,24 @@ export default function OrderHistoryScreen() {
       {/* Active Orders */}
       <ActiveOrders
         activeOrders={activeOrders}
-        isOrdersLoading={networkStatus === 1} // initial load only
+        isOrdersLoading={activeOrderNetwork === 1} // initial load only
       />
 
       {/* Past Orders */}
       <PastOrders
         pastOrders={pastOrders}
-        isOrdersLoading={networkStatus === 1} // initial load only
+        isOrdersLoading={pastOrderNetwork === 1} // initial load only
       />
 
       {/* Load More Button */}
-      {hasMore && (
+      {(activeOrderhasMore || pastOrderhasMore) && (
         <div className="flex justify-center">
           <button
             onClick={loadMore}
-            disabled={loading}
+            disabled={activeOrderLoading || pastOrderLoading}
             className="flex items-center space-x-2 px-6 py-3 bg-primary-color dark:text-black text-white font-semibold rounded-full shadow-md hover:bg-primary-color transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
           >
-            {loading ? (
+            {activeOrderLoading || pastOrderLoading ? (
               <>
                 <svg
                   className="animate-spin h-5 w-5 dark:text-black text-white"
@@ -99,10 +147,10 @@ export default function OrderHistoryScreen() {
                     d="M4 12a8 8 0 018-8v8H4z"
                   ></path>
                 </svg>
-                <span>Loading orders </span>
+                <span>{t("loading_orders")}</span>
               </>
             ) : (
-              <span>Show more orders</span>
+              <span>{t("show_more_orders")}</span>
             )}
           </button>
         </div>
