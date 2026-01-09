f907b1dc7239da1317b3672f5467045c7b4552e4
Muhammad-Ahmad618 <muhammad.ahmad.86.18@gmail.com>
2026-01-09 16:02:31 +0500

    created filter UI

--- enatega-multivendor-admin/lib/ui/screen-components/protected/super-admin/restaurants/view/header/table-header/index.tsx
+++ enatega-multivendor-admin/lib/ui/screen-components/protected/super-admin/restaurants/view/header/table-header/index.tsx
@@ -1,5 +1,6 @@
 // Custom Components
 import CustomTextField from '@/lib/ui/useable-components/input-field';
+import CustomMultiSelectComponent from '@/lib/ui/useable-components/custom-multi-select';
 
 // Interfaces
 import { IRestaurantsTableHeaderProps } from '@/lib/utils/interfaces';
@@ -12,9 +13,20 @@ export default function RestaurantsTableHeader({
   // Hooks
   const t = useTranslations();
 
+  const shopCategoryOptions = [
+    { id: 1, label: 'Category1', code: 'category1' },
+    { id: 2, label: 'Category2', code: 'category2' },
+    { id: 3, label: 'Category3', code: 'category3' },
+  ];
+
+  const shopStatusOptions = [
+    { id: 1, label: t('active'), code: 'active' },
+    { id: 2, label: t('inactive'), code: 'inactive' },
+  ];
+
   return (
     <div className="mb-4 flex flex-col gap-6">
-      <div className="flex-colm:flex-row flex w-fit items-center gap-2">
+      <div className="flex-colm:flex-row flex w-fit flex-wrap items-center gap-2">
         <div className="w-60">
           <CustomTextField
             type="text"
@@ -24,6 +36,29 @@ export default function RestaurantsTableHeader({
             value={globalFilterValue}
             onChange={onGlobalFilterChange}
             placeholder={t('Keyword Search')}
+            className="h-12"
+          />
+        </div>
+        <div>
+          <CustomMultiSelectComponent
+            name="shop_category"
+            options={shopCategoryOptions}
+            selectedItems={[]}
+            setSelectedItems={(key, items) => []}
+            placeholder={t('Shop Category')}
+            className=" w-min border dark:border-dark-600 rounded-md py-1"
+            multiSelectClassName="border-none"
+          />
+        </div>
+        <div>
+          <CustomMultiSelectComponent
+            name="shop_status"
+            options={shopStatusOptions}
+            selectedItems={[]}
+            setSelectedItems={(key, items) => []}
+            placeholder={t('Shop Status')}
+            className=" w-min border dark:border-dark-600 rounded-md py-1"
+            multiSelectClassName="border-none"
           />
         </div>
       </div>
