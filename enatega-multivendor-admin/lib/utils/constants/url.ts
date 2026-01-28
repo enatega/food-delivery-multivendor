00b4b0d3959de1e30b8a4f652a98ded0e1cc39e1
MapMac <mapmac@192.168.1.34>
2026-01-28 14:41:04 +0500

    added the production urls in the .env

--- enatega-multivendor-admin/lib/utils/constants/url.ts
+++ enatega-multivendor-admin/lib/utils/constants/url.ts
@@ -1,10 +1,10 @@
 export const BACKEND_URL = {
   LOCAL: {
-    SERVER_URL: 'http://192.168.1.128:8001/',
-    WS_SERVER_URL: 'ws://192.168.1.128:8001/',
+    SERVER_URL: 'https://62vh1vrf-8001.inc1.devtunnels.ms/',
+    WS_SERVER_URL: 'wss://62vh1vrf-8001.inc1.devtunnels.ms/',
   },
   LIVE: {
-    SERVER_URL: 'http://192.168.1.128:8001/',
-    WS_SERVER_URL: 'ws://192.168.1.128:8001/',
+    SERVER_URL: 'https://62vh1vrf-8001.inc1.devtunnels.ms/',
+    WS_SERVER_URL: 'wss://62vh1vrf-8001.inc1.devtunnels.ms/',
   },
 };
