bff69d2c044b33afd679e395453dfceb328152bc
Muhammad Ali <36534220+ali-20@users.noreply.github.com>
2026-07-16 13:11:34 +0500

    feat: update Google Maps integration and remove backend dependency for API keys

--- enatega-multivendor-rider/environment.ts
+++ enatega-multivendor-rider/environment.ts
@@ -1,9 +1,14 @@
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 import * as Updates from "expo-updates";
-import { useContext } from "react";
+import { Platform } from "react-native";
+import {  useContext } from "react";
 import { ConfigurationContext } from "./lib/context/global/configuration.context";
 const getEnvVars = (env = Updates.channel) => {
   const configuration = useContext(ConfigurationContext);
+  const googleMapsKey =
+    Platform.OS === "ios"
+      ? process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_IOS
+      : process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_ANDROID;
   if (__DEV__) {
     loadDevMessages();
     loadErrorMessages();
@@ -15,7 +20,7 @@ const getEnvVars = (env = Updates.channel) => {
       SENTRY_DSN:
         configuration?.riderAppSentryUrl ??
         "https://e963731ba0f84e5d823a2bbe2968ea4d@o1103026.ingest.sentry.io/6135261",
-      GOOGLE_MAPS_KEY: configuration?.googleApiKey,
+      GOOGLE_MAPS_KEY: googleMapsKey,
       ENVIRONMENT: "production",
     };
   }
@@ -26,7 +31,7 @@ const getEnvVars = (env = Updates.channel) => {
     SENTRY_DSN:
       configuration?.riderAppSentryUrl ??
       "https://e963731ba0f84e5d823a2bbe2968ea4d@o1103026.ingest.sentry.io/6135261",
-    GOOGLE_MAPS_KEY: configuration?.googleApiKey,
+    GOOGLE_MAPS_KEY: googleMapsKey,
     ENVIRONMENT: "development",
   };
 };
